d780bfae918cf7f681269c2d4887681978968263
Rummelhoff7 <mads.jack.rummelhoff@hotmail.com>
2026-05-16 14:46:11 +0200

    requirements osv

--- backend/data/loader.py
+++ backend/data/loader.py
@@ -3,23 +3,28 @@ import os
 from pathlib import Path
 
 def load_data() -> pd.DataFrame:
+
+    #finder csv filer
     raw_path = Path(__file__).parent / "raw"
     
     all_seasons = []
     
+    #Går igennem alle filer i /data/raw
     for file in sorted(raw_path.glob("*.csv")):
+        #Indlæser og kombinere
         df = pd.read_csv(file)
         df["season"] = file.stem  # tilføj sæson-kolonne
         all_seasons.append(df)
     
     combined = pd.concat(all_seasons, ignore_index=True)
     
-    # Behold kun relevante kolonner
+    # Beholder kun relevante kolonner
     cols = ["Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", 
             "FTR", "HTHG", "HTAG", "HS", "AS", "HST", "AST",
             "HC", "AC", "HY", "AY", "HR", "AR", "season"]
     
     combined = combined[[c for c in cols if c in combined.columns]]
+    #fjerner kampe hvor resultat mangler
     combined = combined.dropna(subset=["FTR"])
     
     return combined
